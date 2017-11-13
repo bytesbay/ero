f29ba5ee2f25e3a60d5e66f01a6ccd5073f1a8e4
loshap <gwadecom@gmail.com>
2017-11-13 17:38:20 +0200

    qwe

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	// $('[data-toggle="tooltip"]').tooltip();
+
+	$(window).scroll(function(){
+
+		var offset = $(this).scrollTop();
+
+		if(offset < 300) {
+			$('.page_background').css('filter', 'brightness('+(1 - (offset / 300))+')');
+		}
+
+	});
+
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-109602534-1');
+
+
+});
